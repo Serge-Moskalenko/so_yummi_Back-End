31db641383c1b08ebdcdfb6cc0f84a958a34fc89
Serge-Moskalenko <skmos2019@outlook.com>
2023-03-31 12:24:49 +0100

    start

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+const mongoose = require("mongoose");
+const { DB_HOST }  = process.env;
+
+mongoose.set('strictQuery',true)
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
+}).catch(err => {
+  console.log(err.message);
+  process.exit(1);
+});
+
+
