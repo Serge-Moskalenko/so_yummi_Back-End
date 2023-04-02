d4dd4ed85f50929ce1f9f5191f5d78aa5a0618a8
VitaliiKablukov <99354660+VitaliiKablukov@users.noreply.github.com>
2023-04-03 01:56:58 +0300

    add work

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,6 @@
+const { HttpError } = require("../helpers");
 const ctrlWrapper = require("../helpers/ctrlWrapper");
+const { Ingredient } = require("../models/ingredient");
 const { Recipes } = require("../models/recipes");
 //
 const categoriesType = [
@@ -19,22 +21,74 @@ const categoriesType = [
 ];
 ///////
 const recipesCategory = async (req, res) => {
-  const categories = categoriesType.sort();
+  const categories = [...categoriesType].sort();
   res.json(categories);
 };
 ///////
 ///////
 
-const recipesList = async (req, res) => {};
+const recipesList = async (req, res) => {
+  const recipes = await Recipes.find();
+  if (!recipes) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(recipes);
+};
 ///////
 ///////
-const recipesByCategory = (req, res) => {};
+const recipesByCategory = async (req, res) => {
+  const { category = "Beef" } = req.params;
+  console.log(category);
+  const result = await Recipes.find({ category: { $eq: category } }).limit(8);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 ///////
 ///////
-const recipesById = (req, res) => {};
+const recipesById = async (req, res) => {
+  const { recipesId } = req.params;
+  const recipe = await Recipes.find({ _id: { $eq: recipesId } });
+  if (!recipe) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(recipe);
+};
+const recipesSearch = async (req, res) => {
+  const { pages = 1, limit = 12 } = req.query;
+
+  const { word } = req.params;
+  console.log(pages, limit, word);
+  const skip = (pages - 1) * limit;
+  if (categoriesType.includes(word)) {
+    const ingredientByName = await Ingredient.find({
+      ttl: { $eq: word },
+    });
+    if (!ingredientByName) {
+      throw HttpError(404, "Not found");
+    }
+
+    const recipesByIngredient = await Recipes.find({
+      "ingredients.id": ingredientByName[0]._id,
+    })
+      .skip(skip)
+      .limit(limit);
+    if (!recipesByIngredient) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(recipesByIngredient);
+  } else {
+    const result = await Recipes.find({ title: { $regex: word } })
+      .skip(skip)
+      .limit(limit);
+    res.json(result);
+  }
+};
 module.exports = {
   recipesCategory: ctrlWrapper(recipesCategory),
   recipesList: ctrlWrapper(recipesList),
   recipesByCategory: ctrlWrapper(recipesByCategory),
   recipesById: ctrlWrapper(recipesById),
+  recipesSearch: ctrlWrapper(recipesSearch),
 };
--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -4,12 +4,14 @@ const {
   recipesByCategory,
   recipesList,
   recipesById,
-} = require("../controllers/recipes");
+  recipesSearch,
+} = require("../../controllers/recipes");
 const router = express.Router();
 
 router.get("/category-list", recipesCategory);
 router.get("/main-page", recipesList);
 router.get("/byCategory/:category", recipesByCategory);
 router.get("/byId/:recipesId", recipesById);
+router.get("/search/:word", recipesSearch);
 
 module.exports = router;
