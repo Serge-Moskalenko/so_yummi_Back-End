8d5d216dd23241f8d649aa0ceb355903b9cb6837
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-04-04 11:44:37 -0400

    done favorite

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -1,27 +1,13 @@
 const { HttpError, ctrlWrapper } = require("../helpers");
 const { User } = require("../models/user");
-const { Recipes } = require("../models/recipes");
 
-const add = async (req, res) => {
-  const { _id } = req.user;
-  // const { recipesId } = req.params;
-  const { recipesId } = req.body;
-  const data = await User.findByIdAndUpdate(
-    _id,
-    {
-      $push: { favorite: recipesId },
-    },
-    { new: true }
-  );
-  if (!data) {
-    throw HttpError(404, "Not found");
-  }
-  res.status(200).json(data);
+const getFavoriteRecipe = async (req, res) => {
+  const { favorite } = req.user;
+  res.status(200).json({ favorite });
 };
 
-const remove = async (req, res) => {
+const removeFavoriteRecipe = async (req, res) => {
   const { _id } = req.user;
-  // const { recipesId } = req.params;
   const { recipesId } = req.body;
   const data = await User.findByIdAndUpdate(
     _id,
@@ -33,10 +19,10 @@ const remove = async (req, res) => {
   if (!data) {
     throw HttpError(404, "Not found");
   }
-  res.status(200).json(data);
+  res.status(200).json({ message: "Recepi deleted" });
 };
 
 module.exports = {
-  add: ctrlWrapper(add),
-  remove: ctrlWrapper(remove),
+  getFavoriteRecipe: ctrlWrapper(getFavoriteRecipe),
+  removeFavoriteRecipe: ctrlWrapper(removeFavoriteRecipe),
 };
