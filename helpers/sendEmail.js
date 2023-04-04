8447e23fa23908b718c29042580ee337ad03da94
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2023-04-04 14:21:33 -0400

    fixed subscribe

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,12 +1,12 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, EMAIL_PROJECT } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "soYummy@mail.soYummy.com" };
+  const email = { ...data, from: EMAIL_PROJECT };
   await sgMail.send(email);
   return true;
 };
